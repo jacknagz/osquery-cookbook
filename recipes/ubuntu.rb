f70d0ea4e437c3fe5da0f3950a8a4cae525a0f08
Jack Naglieri <jnaglierijr@gmail.com>
2016-04-12 21:03:15 -0700

    ubuntu12 support, centos kitchen setup

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -7,6 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Example schedule config.
 schedule_config = {
   info: {
     query: 'SELECT * FROM osquery_info',
@@ -14,10 +15,18 @@ schedule_config = {
   }
 }
 
+ubuntu_version = node['platform_version'].split('.')[0].to_i
+case ubuntu_version
+when 14
+  ubuntu_name = 'trusty'
+when 12
+  ubuntu_name = 'precise'
+end
+
 apt_repository 'osquery' do
-  uri 'https://osquery-packages.s3.amazonaws.com/trusty'
+  uri "https://osquery-packages.s3.amazonaws.com/#{ubuntu_name}"
   components ['main']
-  distribution 'trusty'
+  distribution ubuntu_name
   keyserver 'keyserver.ubuntu.com'
   key '1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B'
   action :add
@@ -26,7 +35,7 @@ end
 include_recipe 'apt::default'
 
 package 'osquery' do
-  version "#{node['osquery']['version']}-1.ubuntu14"
+  version "#{node['osquery']['version']}-1.ubuntu#{ubuntu_version}"
   action :install
 end
 
