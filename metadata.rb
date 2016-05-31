0afb136123e4b1125ac5be5841613065a2609cb0
Jack Naglieri <jack.naglieri@airbnb.com>
2016-05-31 13:12:18 -0700

    update metadata for recipes and redhat support

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,15 @@ description 'Install and configure osquery (osquery.io)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
 
-%w(ubuntu centos mac_os_x).each do |os|
+%w(ubuntu centos redhat mac_os_x).each do |os|
   supports os
 end
+
+recipe 'osquery::default', 'convergence/audit requirements.'
+recipe 'osquery::centos', 'centos/redhat osquery installation.'
+recipe 'osquery::mac_os_x', 'mac os x osquery installation.'
+recipe 'osquery::ubuntu', 'ubuntu osquery installation.'
+recipe 'osquery::audit', 'chef audits for osquery installation.'
 
 chef_version '>= 12' if respond_to?(:chef_version)
 issues_url 'https://github.com/jacknagz/osquery-cookbook/issues' if respond_to?(:issues_url)
