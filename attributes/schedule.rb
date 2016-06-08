448ff760fdf0d4c6c89bd975345da5f054cf3819
Jack Naglieri <jnaglierijr@gmail.com>
2016-06-07 22:39:43 -0700

    resolves #36 - remove file_events from base schedule

--- attributes/schedule.rb
+++ attributes/schedule.rb
@@ -3,10 +3,5 @@ default['osquery']['schedule'] = {
   info: {
     query: 'SELECT * FROM osquery_info;',
     interval: 86_400
-  },
-  file_events: {
-    query: 'SELECT * FROM file_events;',
-    removed: false,
-    interval: 300
   }
 }
